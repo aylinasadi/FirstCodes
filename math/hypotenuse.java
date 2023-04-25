ca497ca592048f39fbf14a43ad5fba82c7798c72
sociopathcat <129847708+sociopathcat@users.noreply.github.com>
2023-04-25 19:54:48 +0330

    Add files via upload

---
+++ math/hypotenuse.java
@@ -0,0 +1,28 @@
+package math;
+import java.util.Scanner;
+
+public class hypotenuse {
+
+	public static void main(String[] args) {
+		
+		double a;
+		double b;
+		double c;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("Enter side a: ");
+		a = scanner.nextDouble();
+		System.out.println("Enter side b: ");
+		b = scanner.nextDouble();
+		
+		c = Math.sqrt((a*a)+(b*b));
+		
+		System.out.println("The hypotenuse is "+c);
+		
+		scanner.close();
+
+	}
+
+}
