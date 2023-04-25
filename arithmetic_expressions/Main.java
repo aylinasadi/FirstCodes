91ddd6bd6d3ee2743dbf8955cba231850fc7ce3f
sociopathcat <129847708+sociopathcat@users.noreply.github.com>
2023-04-25 19:53:25 +0330

    Add files via upload

---
+++ arithmetic_expressions/Main.java
@@ -0,0 +1,18 @@
+package arithmetic_expressions;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int apples = 10;
+		apples = apples + 1;
+		apples = apples * 2;
+		apples = apples - 1;
+		apples = apples / 7;
+		apples = apples % 3;   //this gives the remainder of division
+		
+		System.out.println(apples);
+		
+
+	}
+
+}
