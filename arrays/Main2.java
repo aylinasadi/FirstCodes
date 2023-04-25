f3155041272f48c2f5ab8994ba2eec5d922230c6
sociopathcat <129847708+sociopathcat@users.noreply.github.com>
2023-04-25 19:56:48 +0330

    Add files via upload

---
+++ arrays/Main2.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		String[] fruits = new String[3];
+		
+		fruits[0] = "Apple";
+		fruits[1] = "Orange";
+		fruits[2] = "Strawberry";
+		
+		for(int i=0; i<fruits.length; i++) {
+			System.out.println(fruits[i]);
+		}
+
+	}
+
+}
---
+++ wrapper/Main.java
@@ -0,0 +1,25 @@
+package wrapper;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Boolean a = true;
+		Character b = '$';
+		Integer c = 123;
+		Double d = 6.22;
+		String e = "Aylin";
+		
+		if(a==true) {
+			System.out.println("This is true");
+		}
+		
+		if(b=='$') {
+			System.out.println("The character is $");
+		}
+		
+		
+
+	}
+
+}
