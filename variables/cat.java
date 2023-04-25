c1eb2fd5ad3a15fc9df1bee38e725c4f90d59c40
sociopathcat <129847708+sociopathcat@users.noreply.github.com>
2023-04-25 19:43:17 +0330

    Add files via upload

---
+++ variables/cat.java
@@ -0,0 +1,34 @@
+package variables;
+
+public class cat {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is a variables test\n");
+		
+		int a;         //this is a declaration
+		a = 123;       //this is an assignment
+		int b = 122;   //this is an initialization
+		long c = 1234567890234567890L;
+		byte d = 100;
+		float e = 3.141592f;
+		double f = 3.141592653589793;
+		boolean g = true;   //or false
+		char symbol = '$';  //it is possible to use just a letter
+		String name = "Aylin";
+		
+		System.out.println(a);
+		System.out.println("the value of a is "+a);
+		System.out.println(b);
+		System.out.println("the value of b is "+b);
+		System.out.println("the value of c is "+c);
+		System.out.println("the value of d is "+d);
+		System.out.println("the value of e is "+e);
+		System.out.println("the value of f is "+f);
+		System.out.println("the value of g is "+g);
+		System.out.println("the symbol that i want is "+symbol);
+		System.out.println("my name  is "+name);
+
+	}
+
+}
