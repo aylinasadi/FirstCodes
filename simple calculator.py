2742bb56906cc7361c5cde30064382970cd4d406
Aylin <129847708+aylinasadi@users.noreply.github.com>
2025-09-03 15:49:54 +0330

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,19 @@
+operator = input("enter an operator (+ - * /): ")
+
+num1 = float(input("enter the 1st number: "))
+num2 = float(input("enter the 2st number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    result = num1 / num2
+    print(result)
+else:
+    print(f"{operator} is not a valid operator!")
