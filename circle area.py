3b06b8fab9988380964c35fa5dd5a29ebbf503d5
Aylin <129847708+aylinasadi@users.noreply.github.com>
2025-09-03 15:28:39 +0330

    Add files via upload

---
+++ circle area.py
@@ -0,0 +1,9 @@
+import math
+
+radius = float(input("enter the radius of a circle: "))
+
+area = math.pi * pow(radius, 2)
+
+print(f"the area of the circle is: {area}")
+#print(f"the area of the circle is: {round(area, 2)}")
+#print(f"the area of the circle is: {area: .2f}")
