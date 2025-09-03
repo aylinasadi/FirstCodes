4cc7771b0e1d7aef96874a27ab34cc963662abc3
Aylin <129847708+aylinasadi@users.noreply.github.com>
2025-09-03 15:27:37 +0330

    Delete hypotenuse calculator(python).py

--- hypotenuse calculator(python).py
+++
@@ -1,8 +0,0 @@
-import math
-
-a = float(input("enter side a of the triangle: "))
-b = float(input("enter side b of the triangle: "))
-
-c = math.sqrt(pow(a, 2) + pow(b, 2))
-
-print(f"the hypotenuse of the triangle is: {c}")
