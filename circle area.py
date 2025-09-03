af448aae8e82fd7bf1a5ff00d14f9796fc301761
Aylin <129847708+aylinasadi@users.noreply.github.com>
2025-09-03 15:26:33 +0330

    Delete circle area.py

--- circle area.py
+++
@@ -1,9 +0,0 @@
-import math
-
-radius = float(input("enter the radius of a circle: "))
-
-area = math.pi * pow(radius, 2)
-
-print(f"the area of the circle is: {area}")
-#print(f"the area of the circle is: {round(area, 2)}")
-#print(f"the area of the circle is: {area: .2f}")
