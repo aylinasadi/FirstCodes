b962290883dbff5cc28fcd6f2c478c68d956a6cd
aylinasadi <aylinasadi97@gmail.com>
2026-06-05 00:43:24 +0400

    Update Golang folder

--- Golang/test.go
+++ Golang/test.go
@@ -30,4 +30,61 @@ func main() {
 	fmt.Println(g)
 	fmt.Println(h, err)
 
+	if y < 3 {
+		fmt.Println("y is less than 3")
+	} else if y > 3 && y < 10 {
+		fmt.Println("y is between 3 and 10")
+	} else {
+		fmt.Println("y is greater than 10")
+	}
+
+	switch a {
+	case 53:
+		fmt.Println("a is 53")
+	case 54:
+		fmt.Println("a is 54")
+	default:
+		fmt.Println("a is not 53 or 54")
+	}
+
+	switch {
+	case y >= 25:
+		fmt.Println("y is greater than or equal to 25")
+	case y > 15:
+		fmt.Println("y is greater than 15")
+	default:
+		fmt.Println("y is less than or equal to 15")
+	}
+
+	switch {
+	case y > 10:
+		fmt.Println("y is greater than 10")
+		fallthrough
+	case y > 18:
+		fmt.Println("y is greater than 18")
+		fallthrough
+	case y > 19:
+		fmt.Println("y is greater than 19")
+	default:
+		fmt.Println("y is less than or equal to 10")
+	}
+
+	switch x {
+	case "hello", "hi", "hey":
+		fmt.Println("x is a greeting")
+	default:
+		fmt.Println("x is not a greeting")
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	for y > 15 {
+		fmt.Print("ha")
+		y--
+	}
+	fmt.Println()
+
+	fmt.Println(string(x[0]))
 }
